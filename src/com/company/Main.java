c43f27d90b0e133609c44a51aef89da5dee09ca3
Pooja Sree <you@example.com>
2022-02-17 22:43:45 +0530

    Added: Addition of two numbers logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to github");
+        int a=1;
+        int b=2;
+        int c= a+b;
+        System.out.println("c= "+c);
 
     }
 }
